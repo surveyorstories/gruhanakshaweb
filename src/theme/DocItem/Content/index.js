6bf248a174a547cbf98638cf8c9e458825ac9675
Lokesh Metta <26522232+lokeshmetta@users.noreply.github.com>
2026-08-06 09:30:16 +0530

    deploy

--- src/theme/DocItem/Content/index.js
+++ src/theme/DocItem/Content/index.js
@@ -32,7 +32,7 @@ function AdComponent() {
       <ins className="adsbygoogle"
         style={{ display: 'block' }}
         data-ad-client="ca-pub-5740737782081297"
-        data-ad-slot="1234567890" // TODO: Replace with your real ad slot ID
+        data-ad-slot="7599347439"
         data-ad-format="auto"
         data-full-width-responsive="true"></ins>
     </div>
